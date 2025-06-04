d317695844a26329c77893496d41eeba82039984
mmarcolino <matheuseduardoleitemarcolino@gmail.com>
2025-06-04 09:04:40 -0300

    add db connection

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"github.com/marcolino/jukebox/gen/openapi"
 	"github.com/marcolino/jukebox/internal/api"
+	"github.com/marcolino/jukebox/internal/resources/database/postgres"
 )
 
 func main() {
-	handler := api.Handler{}
+	// db_url := os.Getenv("DATABASE_URL")
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
 
-	server, err := openapi.NewServer(&handler)
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	moduleHandler := postgres.New(db)
+	handler := api.NewHandler(moduleHandler)
+
+	server, err := openapi.NewServer(handler)
 	if err != nil {
 		log.Fatal(err)
 	}
