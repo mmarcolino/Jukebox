b4bd296ad26786794e935b6e46f8cf9ef7b79fff
mmarcolino <matheuseduardoleitemarcolino@gmail.com>
2025-06-11 08:27:18 -0300

    added errors to api

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,6 +1,12 @@
 package api
 
-import "github.com/marcolino/jukebox/internal/domain/gateway"
+import (
+	"context"
+
+	"github.com/marcolino/jukebox/gen/openapi"
+	"github.com/marcolino/jukebox/internal/domain/entity"
+	"github.com/marcolino/jukebox/internal/domain/gateway"
+)
 
 type Handler struct {
 	tracksHandler gateway.Tracks
@@ -8,4 +14,23 @@ type Handler struct {
 
 func NewHandler(module gateway.Tracks) *Handler {
 	return &Handler{tracksHandler: module}
+}
+
+func (h *Handler) NewError(ctx context.Context, err error) *openapi.ErrorResponseOgenStatusCode {
+	statusCode := 500
+	errMsg := "INTERNAL_ERROR"
+
+	switch err.Error() {
+	case "NOT_FOUND":
+		statusCode = 404
+		errMsg = entity.ErrNotFound.Error()
+	}
+
+	return &openapi.ErrorResponseOgenStatusCode{
+		StatusCode: statusCode,
+		Response: openapi.ErrorResponseOgen{
+			ErrorMessage: errMsg,
+			StatusCode:   statusCode,
+		},
+	}
 }
